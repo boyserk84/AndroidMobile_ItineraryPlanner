42f5b423e1cfe02b9cb146da4ba66e3d2f702d87
Nate K <nkemavaha@zynga.com>
2014-04-26 23:52:40 -0700

    [ Android Mobile App | Travel Planner ] Implementing framework FTUE with swipe (viewPager).

--- src/com/codepath/travelplanner/activities/FtueActivity.java
+++ src/com/codepath/travelplanner/activities/FtueActivity.java
@@ -1,28 +1,75 @@
 package com.codepath.travelplanner.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.codepath.travelplanner.R;
+import com.codepath.travelplanner.adapters.FtueFragementAdapter;
+
 
 /**
  * FtueActivity
  * First-Time User Experience Activity
  * 
  * Only shows for first time user.
+ * 
+ * TODO: Need persistent or session data to save Ftue Flag.
  * @author nkemavaha
  *
  */
-public class FtueActivity extends Activity {
+public class FtueActivity extends FragmentActivity {
+	
+	private FtueFragementAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ftue);
+		setupViews();
+	}
+	
+	/**
+	 * Setup views and setup event listeners for this activity
+	 */
+	private void setupViews() {
+		ViewPager pager = (ViewPager) findViewById(R.id.vpPager);
+
+		adapter = new FtueFragementAdapter( getSupportFragmentManager() );
+		if ( pager != null ) {
+			pager.setAdapter( adapter );
+			pager.setOnPageChangeListener( new OnPageChangeListener() {
+				
+				@Override
+				public void onPageSelected(int page) {}
+				
+				@Override
+				public void onPageScrolled(int pos, float arg1, int arg2) {
+					Fragment ftueFragment = adapter.getItem( pos );
+					FragmentManager manager = getSupportFragmentManager();
+					
+					// use appropriate transaction for backward compatibility
+					FragmentTransaction fts = manager.beginTransaction();
+					
+					fts.replace( R.id.flFtueContainer , ftueFragment);
+					
+					// commit and update changes to fragment
+					fts.commit();
+					
+				}
+				
+				@Override
+				public void onPageScrollStateChanged(int arg0) {}
+			});
+		}	
 	}
 
 	@Override
@@ -33,12 +80,13 @@ public class FtueActivity extends Activity {
 	}
 	
 	/**
-	 * Callback to skip FTUE
+	 * Callback to skip FTUE and go straight to main activity
 	 * @param v
 	 */
 	public void onSkipPressed(View v) {
 		Intent i = new Intent( getBaseContext(), MainActivity.class);
 		startActivity( i );
+		this.finish();
 	}
 
 	@Override
