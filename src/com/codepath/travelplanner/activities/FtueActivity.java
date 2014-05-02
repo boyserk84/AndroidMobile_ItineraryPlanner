e363866cbce404653120ca2e5e5bab9ef81a51b8
Nate K <nkemavaha@zynga.com>
2014-05-01 23:23:42 -0700

    [ Android Mobile App | Travel Planner ] Show FTUE only once activated.

--- src/com/codepath/travelplanner/activities/FtueActivity.java
+++ src/com/codepath/travelplanner/activities/FtueActivity.java
@@ -56,7 +56,7 @@ public class FtueActivity extends FragmentActivity implements IActivityListener 
 		
 		// If already done FTUE or currently NOT in the help mode, just skip it.
 		if ( isInHelpMode == false && ftueSharePref.getBoolean( DONE_FTUE_SETTING_KEY , false) == true ) {
-			//onSkipPressed( null );
+			onSkipPressed( null );
 		}
 	}
 	
